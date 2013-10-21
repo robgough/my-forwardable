e066aab766dca7792790d119112a0ae37fe68ec6
Rob Gough <robert.gough@onthebeach.co.uk>
2013-10-21 23:27:18 +0100

    Tests pass, but thanks to hard coded `records`
    Need to figure out how to actually send to a variable on the class
    without using it directly. I'm thinking I can use send for this?

--- lib/my_forwardable.rb
+++ lib/my_forwardable.rb
@@ -1,3 +1,19 @@
 module MyForwardable
+  def def_delegator(receiver, method_name, method_alias = method_name)
+    add_delegator(receiver, method_name, method_alias)
+  end
 
+  def def_delegators(receiver, *args)
+    args.each do |arg|
+      add_delegator(receiver, arg)
+    end
+  end
+
+  private 
+
+  def add_delegator(receiver, method_name, method_alias = method_name)
+    define_method(method_alias) do |*args, &block|
+      self.records.__send__(method_name, *args, &block)      
+    end
+  end
 end
